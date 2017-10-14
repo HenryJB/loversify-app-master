87ae8efd264ed9a54f7bd1914911d05dc1ea61a7
alexonozor <alexonozor@gmail.com>
2017-10-14 09:00:01 +0100

    feat(resoures):  add resources page

---
+++ src/pages/resoures-pages/resoures-pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ResouresPagesPage } from './resoures-pages';
+
+@NgModule({
+  declarations: [
+    ResouresPagesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ResouresPagesPage),
+  ],
+})
+export class ResouresPagesPageModule {}
