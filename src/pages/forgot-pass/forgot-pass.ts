51e8e4200adf7360f76e037592cf57bd819d84b3
alexonozor <alexonozor@gmail.com>
2017-10-16 20:07:13 +0100

    feat(forget password): add forget password

--- src/pages/forgot-pass/forgot-pass.ts
+++ src/pages/forgot-pass/forgot-pass.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms'
+import { AuthProvider } from '../../providers/auth/auth';
+import { SharedProvider } from '../../providers/shared/shared';
 
 @IonicPage()
 @Component({
@@ -9,17 +10,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'forgot-pass.html',
 })
 export class ForgotPassPage {
+  public restPasswordForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public fb: FormBuilder,
+    public _authService: AuthProvider,
+    public _sharedService: SharedProvider
+  )
+   {  
+     this.createForm();
+   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ForgotPassPage');
   }
 
-  goto(page:string){
-    this.navCtrl.push(page);
+  createForm() {
+    this.restPasswordForm = this.fb.group({
+      email: ['', Validators.required]
+    })
   }
+
+  resetPassWord() {
+    let loader = this._sharedService.loader();
+    loader.present().then(() => {
+      this._authService.resetPassword(this.restPasswordForm.value)
+        .subscribe((resp) => {
+          if (resp.success) {
+            this.restPasswordForm.reset();
+            this._sharedService.toaster('Password rest information has been sent to your email');
+            loader.dismiss();
+          } else {
+            loader.dismiss();
+            this._sharedService.toaster('internal server error');
+          }
+        }, (err) => {
+          // Unable to log in
+          loader.dismiss();
+          this._sharedService.toaster('internal server error');
+      });
+    })
+  }
+
+
+  
 
 
 }
